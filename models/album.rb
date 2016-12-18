d2546e9b8cf21c5df7478b6ad9d3d56942429d5e
Steven Noble <steven.noble20@gmail.com>
2016-12-18 20:29:43 +0000

    add modules

--- models/album.rb
+++ models/album.rb
@@ -2,6 +2,49 @@ require_relative('../db/sql_runner')
 
 class Album
 
+  def initialize (options)
+    @id = options['id'].to_i || nil
+    @name = options['name']
+    @genre = options['genre']
+    @quantity = options['quantity']
+    @artist_id = otions['artist_id']
+  end
+
+  def save ()
+    sql = "INSERT INTO albums (name , genre , quantity ) VALUES ('#{@name}' , '#{@genre}', #{@quantity}, ) returning *; "
+    @id = SqlRunner.run(sql)[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "
+      SELECT * FROM albums;
+    "
+    all_albums = SqlRunner.run(sql).map { |album| Album.new(album) }
+    return all_albums
+  end
+
+  def self.find_by_id(id)
+    sql = "
+      SELECT * FROM albums
+      WHERE id = #{id};
+    "
+    album = SqlRunner.run(sql)[0]
+    return album
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM albums;"
+    SqlRunner.run(sql)
+  end
+
+  def self.artist_name(id)
+    sql = "SELECT artist.name FROM albums WHERE id = #{id};"
+    artist_name = SqlRunner.run(sql)[0]
+    return artist_name
+  end
+
+
+
 
 
 end
--- models/artist.rb
+++ models/artist.rb
@@ -2,4 +2,10 @@ require_relative('../db/sql_runner')
 
 class Artist
 
+  def initialize (options)
+    @id = options['id'].to_i || nil
+    @name = options['name']
+  end
+
+
 end
